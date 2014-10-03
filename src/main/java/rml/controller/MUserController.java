1e01588e7917c416df7bcee824ac428464e86c2d
gus <125822741@qq.com>
2014-10-03 19:30:38 +0800

    update

--- src/main/java/rml/controller/MUserController.java
+++ src/main/java/rml/controller/MUserController.java
@@ -5,17 +5,22 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import rml.model.MUser;
 import rml.service.MUserServiceI;
 
 @Controller
-@RequestMapping("/muserController")
+@RequestMapping("/")
 public class MUserController {
-
+	private final Logger log = Logger.getLogger(MUserController.class);
+	
 	private MUserServiceI muserService;
 
 	public MUserServiceI getMuserService() {
@@ -27,33 +32,45 @@ public class MUserController {
 		this.muserService = muserService;
 	}
 	
-	@RequestMapping(value="/listUser")
-	public String listUser(HttpServletRequest request) {
+	
+	/**
+	 * 微信回调接口
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value="callback", method=RequestMethod.GET)
+	public @ResponseBody String callback(HttpServletRequest request) {
+		log.debug("callback method!" + request.getParameter("echostr"));
 		
-		List <MUser> list = muserService.getAll();
-		request.setAttribute("userlist", list);
-		return "listUser";
+		return request.getParameter("echostr");
+		//return "Mapped by path pattern ('" + request.getRequestURI() + "')";
 	}
 	
-	@RequestMapping(value="/addUser")
+	@RequestMapping("listUser")
+	public String listUser(Model model) {
+		System.out.println("==================================================================================");
+		List <MUser> list = muserService.getAll();
+		model.addAttribute("userlist", list);
+		return "views/listUser";
+	}
+	
+	@RequestMapping(value="addUser")
 	public String addUser(MUser muser) {
-			
 		String id = UUID.randomUUID().toString();
 		muser.setId(id);
 		muserService.insert(muser);
-		return "redirect:/muserController/listUser.do";
+		return "redirect:/listUser";
 	}
 	
 	@RequestMapping(value="/deleteUser")
 	public String deleteUser(String id) {
 		
 		muserService.delete(id);
-		return "redirect:/muserController/listUser.do";
+		return "redirect:/listUser";
 	}
 	
 	@RequestMapping(value="/updateUserUI")
 	public String updateUserUI(String id, HttpServletRequest request) {
-		
 		MUser muser = muserService.selectByPrimaryKey(id);
 		request.setAttribute("user", muser);
 		return "updateUser";
@@ -63,6 +80,6 @@ public class MUserController {
 	public String updateUser(MUser muser) {
 		
 		muserService.update(muser);
-		return "redirect:/muserController/listUser.do";
+		return "redirect:/listUser.do";
 	}
 }
--- src/main/webapp/WEB-INF/views/views/index.jsp
+++ src/main/webapp/WEB-INF/views/views/index.jsp
@@ -1,28 +1,23 @@
-<%@ page language="java" import="java.util.*" pageEncoding="ISO-8859-1"%>
-<%
-String path = request.getContextPath();
-String basePath = request.getScheme()+"://"+request.getServerName()+":"+request.getServerPort()+path+"/";
-%>
-
+<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
 <html>
   <head>
-    <base href="<%=basePath%>">
-    
-    <title>My JSP 'index.jsp' starting page</title>
-	<meta http-equiv="pragma" content="no-cache">
-	<meta http-equiv="cache-control" content="no-cache">
-	<meta http-equiv="expires" content="0">    
-	<meta http-equiv="keywords" content="keyword1,keyword2,keyword3">
-	<meta http-equiv="description" content="This is my page">
-	<!--
-	<link rel="stylesheet" type="text/css" href="styles.css">
-	-->
+    <title>送礼</title>
+	<link href="<c:url value="/resources/script/jquery-1.7.2.min.js" />" rel="stylesheet"  type="text/css" />
+	<link href="<c:url value="/resources/script/lotteryCore.js" />" rel="stylesheet"  type="text/css" />
+	<link href="<c:url value="/resources/script/scratch.js" />" rel="stylesheet"  type="text/css" />
   </head>
   
   <body>
-    Spring core + Spring MVC + MyBatis <br>
-    
-    <a href="<%=path %>/muserController/listUser.do">User List</a>
+    抽奖程序<br>
+    <img alt="" src="<c:url value="/resources/images/tomcat.gif" />" />
+    <a href="listUser.do">User List</a>
   </body>
 </html>
+
+http://mp.weixin.qq.com/mp/redirect?
+url=https%3A%2F%2Fopen.weixin.qq.com%2Fconnect%2F
+oauth2%2Fauthorize%3F
+appid%3Dwx8cbf7e0e11903570%26
+redirect_uri%3Dhttp%253A%252F%252Fwx.aisidi.com%252Fasdwx%252FcallBack%252Fweixin.do%253F%26response_type%3DCODE%26scope%3Dsnsapi_base%26state%3DASDSCRATCH%23wechat_redirect
